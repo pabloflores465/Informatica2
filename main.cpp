24b8223af8014006ef7b7e17dcf1019cec528c9c
Pablo Flores <pablopolis2016@gmail.com>
2022-10-06 11:58:22 -0600

    Areglando el módulo 2

--- main.cpp
+++ main.cpp
@@ -34,8 +34,7 @@ int main() {
 
     //Modulo 1
     //Existencias de los farmacos
-    double existenciasfarmacos[10]{20,20,20,20,20,20,20,20,20,20
-    };
+    double existenciasfarmacos[10]{20,20,20,20,20,20,20,20,20,20};
 
     //Modulo 2
     //Aseguradoras Usadas
@@ -315,8 +314,7 @@ int main() {
                     <<"2=Aseguradora General, S.A.\n"
                     <<"3=Seguros el Roble\n"
                     <<"4=Mapfre\n";
-                getline(cin,nombre[lola]);
-                lola++;
+                    cin>>seguro;
                 //Aqui se Muestra la informacion de la aseguradora seleccionada
                 switch(seguro) {
                     //Seguros GyT
