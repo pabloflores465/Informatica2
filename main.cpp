381e98e4595510794a1f6175ecbb0ef5728d5bc8
Pablo Flores <pablopolis2016@gmail.com>
2022-10-23 11:59:36 -0600

    Version Alfa 2.6
    Cambios errores

--- main.cpp
+++ main.cpp
@@ -116,10 +116,9 @@ int main() {
     //Esta es la contraseña del Cliente
     string contraDep;
 
-   /* cout<<"Hola administrador\n"<<
-        "Por favor realiza la configuración inicial de los usuarios."<<endl;*/
-
-    /*do{
+   cout<<"Hola administrador\n"<<
+        "Por favor realiza la configuración inicial de los usuarios."<<endl;
+   do{
         //Ingresan el usuario
         cout<<"Usuario"<<endl;
         cin>>nombreADMIN;
@@ -132,9 +131,9 @@ int main() {
         else {
             cout<<"ERROR: Usuario o Contraseña de administrador incorrectos"<<endl;
         }
-    }*/
-//    while (nombreADMIN!="ADMIN"||contraADMIN!="admin");
-//
+    }
+    while (nombreADMIN!="ADMIN"||contraADMIN!="admin");
+
     do{
         cout<<"Bienvenido a Farmacias Batres"<<endl;
         cout<<"¿Quien es usted?\n"
