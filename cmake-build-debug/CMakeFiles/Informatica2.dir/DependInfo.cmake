f0fd42db8aeba1361daa8f440351bf601c832071
Pablo Flores <pablopolis2016@gmail.com>
2022-10-24 11:44:44 -0600

    hola

--- cmake-build-debug/CMakeFiles/Informatica2.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/Informatica2.dir/DependInfo.cmake
@@ -8,7 +8,7 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/Users/randyrivera/Documents/Informatica2/main.cpp" "CMakeFiles/Informatica2.dir/main.cpp.o" "gcc" "CMakeFiles/Informatica2.dir/main.cpp.o.d"
+  "/home/pabloflores/Documentos/Informatica2/main.cpp" "CMakeFiles/Informatica2.dir/main.cpp.o" "gcc" "CMakeFiles/Informatica2.dir/main.cpp.o.d"
   )
 
 # Targets to which this target links.
