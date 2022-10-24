9869cce858f742d33a9c3f766164ea8f90f37d4f
rmrandy <88206040+rmrandy@users.noreply.github.com>
2022-10-23 21:50:04 -0600

    Prueba del modulo 3

---
+++ cmake-build-debug/CMakeFiles/Informatica2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Informatica2.dir/main.cpp.o"
+  "CMakeFiles/Informatica2.dir/main.cpp.o.d"
+  "Informatica2"
+  "Informatica2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Informatica2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Informatica2.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Informatica2.
+# This may be replaced when dependencies are built.
